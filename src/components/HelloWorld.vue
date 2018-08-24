ed28ae17f81952d725dc6e6ede8b2c4adfee514e
asmsuechan <suenagaryoutaabc@gmail.com>
2018-08-24 15:40:59 +0900

    Update contents

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,6 +9,36 @@
         <li><p><a href='https://twitter.com/suenaga_ryota'>Twitter</a></p></li>
       </ul>
     </div>
+
+    <hr>
+
+    <h1>Education</h1>
+    <span>Yame-gakuin junior-high school, 2008-2011</span><br>
+    <span>Yame-gakuin high school, 2011-2014</span><br>
+    <span>Meiji University IMS Network Design, 2014-2019</span><br>
+    <hr>
+
+    <h1>Skills</h1>
+    <div>
+      <div>
+        <strong>Languages</strong><br>
+        <span>Ruby / JavaScript / Golang / Regex / Japanese / English / Chinense</span>
+      </div>
+      <div>
+        <strong>Frameworks</strong><br>
+        <span>Ruby on Rails / jQuery / Vue.js / React / react-native / electron</span>
+      </div>
+      <div>
+        <strong>Clouds</strong><br>
+        <span>AWS / Alibaba Cloud / Heroku</span>
+      </div>
+      <div>
+        <strong>Others</strong><br>
+        <span>Serverless / JWT / REST / GraphQL</span>
+      </div>
+    </div>
+    <hr>
+
     <h1>Projects</h1>
     <div>
       <h3>Boostnote</h3>
@@ -20,6 +50,7 @@
         <span>[Technologies] Electron, React, travis-ci, GitHub, Slack</span>
       </div>
     </div>
+    <hr>
 
     <h1>Experiences</h1>
     <div>
@@ -41,6 +72,8 @@
         <span>[Technologies] Ruby on Rails, jQuery, Vue.js, RSpec, CircleCI, GitHub, Slack</span>
       </div>
     </div>
+    <hr>
+
     <h1>Activities</h1>
     <div>
       <h3>Silicon Valley Workshop</h3>
@@ -49,7 +82,19 @@
         <span>I participated in <a href="http://techlabpaak.com/siliconvalleyworkshop2018">Silicon Valley Workshop</a> held by Recruit.</span>
       </div>
     </div>
-    <div class="footer"></div>
+    <hr>
+
+    <h1>About This Page</h1>
+    <div>
+      <div>
+        <span>This page runs on CloudFront and S3 and be written in Vue.js. TravisCI deploys to S3 automatically when any commits found on <a href="https://github.com/asmsuechan/asmsuechan.com">GitHub respository</a>.</span>
+      </div>
+    </div>
+    <hr>
+
+    <div class="footer">
+      <span>Copyright (c) 2018 asmsuechan All Rights Reserved.</span>
+    </div>
   </div>
 </template>
 
@@ -81,6 +126,12 @@ a {
   color: #42b983;
 }
 .footer {
+  margin-top: 50px;
   height: 50px;
+}
+hr {
+  width: 20%;
+  margin-top: 30px;
+  margin-bottom: 30px;
 }
 </style>
