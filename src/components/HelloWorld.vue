1b18dc11782238c3bee72035bbabcaa40c9e2f69
asmsuechan <suenagaryoutaabc@gmail.com>
2018-08-24 14:21:50 +0900

    Update contents

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,6 +9,47 @@
         <li><p><a href='https://twitter.com/suenaga_ryota'>Twitter</a></p></li>
       </ul>
     </div>
+    <h1>Projects</h1>
+    <div>
+      <h3>Boostnote</h3>
+      <b>Developer</b>
+      <span>2017/1 - 2018/1</span>
+      <div>
+        <span>I was one of the committers of <a href='https://github.com/BoostIO/Boostnote'>Boostnote</a> which is a note-taking application for programmers.</span>
+        <br>
+        <span>[Technologies] Electron, React, travis-ci, GitHub, Slack</span>
+      </div>
+    </div>
+
+    <h1>Experiences</h1>
+    <div>
+      <h3>SUPINF</h3>
+      <b>Backend/Frontend Developer</b>
+      <span>2017/5 - 2018/5</span>
+      <div>
+        <span>I was in charge of Backend/Frontend of an SAP web application which is a business communication/task management tool.</span>
+        <br>
+        <span>[Technologies] Ruby on Rails, Vue.js, RSpec, vue-test-utils, swagger, JWT, REST, CircleCI, GitHub, Slack</span>
+      </div>
+
+      <h3>CrowdWorks</h3>
+      <b>Backend Developer Internship</b>
+      <span>2016/5 - 2017/3</span>
+      <div>
+        <span>I was in charge of Backend development. The main project I was in charge of was implementation of Messanger-like desktop notification by Serviceworker.</span>
+        <br>
+        <span>[Technologies] Ruby on Rails, jQuery, Vue.js, RSpec, CircleCI, GitHub, Slack</span>
+      </div>
+    </div>
+    <h1>Activities</h1>
+    <div>
+      <h3>Silicon Valley Workshop</h3>
+      <span>2016/3 - 2016/3 (10 days)</span>
+      <div>
+        <span>I participated in <a href="http://techlabpaak.com/siliconvalleyworkshop2018">Silicon Valley Workshop</a> held by Recruit.</span>
+      </div>
+    </div>
+    <div class="footer"></div>
   </div>
 </template>
 
@@ -38,5 +79,8 @@ li {
 }
 a {
   color: #42b983;
+}
+.footer {
+  height: 50px;
 }
 </style>
